ac4d00026d792c905917b3e8139cf35cc77a68c4
swordcube <49110074+swordcube@users.noreply.github.com>
2021-02-26 17:43:03 -0600

    Removed references to bot token
    Removed references to bot token in config.json and all other js files

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const { prefix, version, botLogo, embedcolor } = require('../config.json')
+const { version, botLogo, embedcolor } = require('../config.json')
 
 module.exports = {
 	name: 'help',
