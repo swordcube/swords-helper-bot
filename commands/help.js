9a03dea5004675d2c18080f818055bec426de1e6
swordcube <49110074+swordcube@users.noreply.github.com>
2021-03-01 22:18:32 -0600

    Updated help.js & Created invite.js

--- commands/help.js
+++ commands/help.js
@@ -15,19 +15,19 @@ module.exports = {
 			.setTitle("Help")
 			.setDescription(`**These are the list of commands for Sword's Helper:**
 
-			${prefix}help - Brings up this message.
-			${prefix}ping - Tells you the ping of the bot.
-			${prefix}clap - Repeats what you say, but with claps in-between.
-			${prefix}echo - Repeats what you say.
-			${prefix}kill - Kills the person you @mention.
-			${prefix}kick - Kicks the person you @mention from the server.
-			${prefix}ban - Bans the person you @mention from the server.
-			${prefix}unban - Unbans the person you provide a user ID for from the server.
-			${prefix}purge - Purges/clears an amount of messages that you specify.
-			${prefix}reboot - Reboots/restarts the bot. (Only the bot owner can use this)
+${prefix}help - Brings up this message.
+${prefix}ping - Tells you the ping of the bot.
+${prefix}clap - Repeats what you say, but with claps in-between.
+${prefix}echo - Repeats what you say.
+${prefix}kill - Kills the person you @mention.
+${prefix}kick - Kicks the person you @mention from the server.
+${prefix}ban - Bans the person you @mention from the server.
+${prefix}unban - Unbans the person you provide a user ID for from the server.
+${prefix}purge - Purges/clears an amount of messages that you specify.
+${prefix}reboot - Reboots/restarts the bot. (Only the bot owner can use this)
 
-			**The bot is open source! Click the link below to make your own bot like mine!**
-			Source code on GitHub: https://github.com/swordcube/swords-helper-bot
+**The bot is open source! Click the link below to make your own bot like mine!**
+Source code on GitHub: https://github.com/swordcube/swords-helper-bot
 			`)
 			.setFooter(`v${version} - Created by hexianimates / swordcube`, botLogo)
 			.setThumbnail(botLogo)
---
+++ commands/invite.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const { prefix, version, botLogo, embedcolor } = require('../config.json')
+
+module.exports = {
+	name: 'invite',
+	description: "Brings up the bot's invite",
+	async execute(message, args){
+
+		//start of code
+		try {
+		const msg = args.join(' ')
+
+		const helpembed = new Discord.MessageEmbed()
+
+			.setTitle("Invite")
+			.setDescription(`Want to invite the bot to your server?
+Click the link below:
+https://discord.com/oauth2/authorize?client_id=766645902491648010&scope=bot&permissions=805314622`)
+			.setFooter(`v${version} - Created by hexianimates / swordcube`, botLogo)
+			.setThumbnail(botLogo)
+			.setColor(embedcolor)
+
+			message.channel.send(helpembed)
+		}
+		catch {
+		
+		};
+		//end of code
+
+	}
+}
