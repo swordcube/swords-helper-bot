0a7bb584df4b06b2b2c896030989b510be195718
swordcube <49110074+swordcube@users.noreply.github.com>
2021-03-03 12:40:06 -0600

    Create userinfo.js

---
+++ commands/userinfo.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const { prefix, version, botLogo, embedcolor } = require('../config.json')
+
+module.exports = {
+	name: 'userinfo',
+	description: "Brings up the info of a mentioned user.",
+	async execute(message, args){
+
+		//start of code
+		try {
+		const { guild, channel } = message
+
+		const user = message.mentions.user.first() || message.member.user
+		const member = guild.members.cache.get(user.id)
+
+		
+
+		const userembed = new Discord.MessageEmbed()
+
+			.setAuthor(`User info for ${user.username}`, user.displayAvatarURL())
+			.setColor(embedcolor)
+
+			message.channel.send(userembed)
+		}
+		catch {
+		
+		};
+		//end of code
+
+	}
+}
