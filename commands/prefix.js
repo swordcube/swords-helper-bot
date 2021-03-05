239d423ef4c769da6e62420907c4413fd9645f14
swordcube <49110074+swordcube@users.noreply.github.com>
2021-03-05 01:57:46 -0600

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -7,12 +7,12 @@ module.exports = {
 	async execute(message, args){
 
 		//start of code
-		async try {
+		try {
 			if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send(":x: You don't have the permissions to change the server prefix.");
 			if(!args[0]) return message.channel.send(":x: You didn't tell me what to change the prefix to.");
 
 
-			await db.set(`prefix_${message.guild.id}`, args[0])			
+			db.set(`prefix_${message.guild.id}`, args[0])			
 
 			const embed = new Discord.MessageEmbed()
 
