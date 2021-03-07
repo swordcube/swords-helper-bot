259444259acec25423c3bc2e16f8de76ff67eed6
swordcube <49110074+swordcube@users.noreply.github.com>
2021-03-06 23:57:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 //const db = require('quick.db');
 const db = require('mongoose')
 
-const dbOptions {
+const dbOptions = {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useFindAndModify: true
