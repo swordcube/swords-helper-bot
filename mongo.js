c3cf2654503ccbb185f907497d388005168d6557
swordcube <49110074+swordcube@users.noreply.github.com>
2021-03-07 00:26:04 -0600

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose')
 
 const dbOptions = {
 	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useFindAndModify: true
+	useUnifiedTopology: true
 }
 
 module.exports = async () => {
