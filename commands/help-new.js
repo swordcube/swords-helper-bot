441f73d992dd01f1b19424550a6e18bcffbe1f7e
swordcube <49110074+swordcube@users.noreply.github.com>
2021-03-17 02:58:33 -0500

    Update help-new.js

--- commands/help-new.js
+++ commands/help-new.js
@@ -1,23 +1,21 @@
-const Discord = require("discord.js")
-const { prefix, version, botLogo, embedcolor } = require('../config.json');
+const { MessageEmbed } = require("discord.js");
+// const { prefix, version, botLogo, embedcolor } = require('../config.json');
 
 module.exports = {
 	name: "help-new",
 	description: "Brings up this message.",
 
 	execute(client, message, args) {
-		if(!args[0]) {
-			const helpembed = new Discord.MessageEmbed()
-			
-			.setTitle(Help)
-			.setDescription("Here is a list of all available commands for Sword's Helper.")
-			.setColor(embedcolor)
-		
-		client.commands.forEach((command) => {
-			helpembed.addField(command.name, command.description)
-		});
+		if (!args[0]) {
+			const embed = new MessageEmbed()
+				.setColor("BLUE")
+				.setTitle(Help);
 
-		message.channel.send(helpembed)
+			client.commands.forEach((command) => {
+				embed.addField(command.name, command.description);
+			});
+
+			message.channel.send(embed);
 		}
 	},	//end of code
 };
