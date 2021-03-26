78906e4bd1007bf8c17f6945c91908f03c1b3914
swordcube <49110074+swordcube@users.noreply.github.com>
2021-03-25 21:07:49 -0500

    Added a new command (sw?bih) and updated the rip and bih commands to auto generate the year

--- commands/bih.js
+++ commands/bih.js
@@ -3,7 +3,7 @@ const { prefix, version, botLogo, embedcolor } = require('../config.json')
 const currentdate = new Date()
 
 module.exports = {
-	name: 'rip',
+	name: 'bih',
 	description: "Generates a tombstone.",
 	async execute(message, args){
 
@@ -13,9 +13,8 @@ module.exports = {
 
 		const ripembed = new Discord.MessageEmbed()
 
-			.setImage(`http://www.tombstonebuilder.com/generate.php?top1=R.I.P&top2=&top3=${msg}&top4=1000+-+${currentdate.getFullYear()}&sp=`)
+			.setImage(`http://www.tombstonebuilder.com/generate.php?top1=B.I.H&top2=&top3=${msg}&top4=1000+-+${currentdate.getFullYear()}&sp=`)
 			.setColor(embedcolor)
-      // console.log(currentdate.getFullYear())
 
 			if (!msg) return message.channel.send(":x: You didn't type in anything. Try again.");
 
